cabbac12cb6d46c1b1eb655c55eea6f413d9a267
the creator <chenxi.yan@gmail.com>
2023-09-26 21:24:35 -0400

    New changes to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Software Developer | Chris Yan',
+  title: 'AI & Software | Chris Yan',
   description: 'Chris is a software and machine learning engineer currenly studying at UofT.',
 }
 
