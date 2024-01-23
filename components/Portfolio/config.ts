0c92b123b1b31c0af6b1d94298662c239512145c
the creator <chenxi.yan@gmail.com>
2024-01-22 22:44:30 -0500

    feat: complete splash screen

--- components/Portfolio/config.ts
+++ components/Portfolio/config.ts
@@ -1,4 +1,4 @@
-import Difenda from '@/public/Work/difenda.png'
+import Temp from '@/public/temp.webp';
 import { StaticImageData } from 'next/image';
 
 export interface ProjectDataProps {
@@ -12,21 +12,21 @@ export interface ProjectDataProps {
 export const projectData = [
   {
     id: 1,
-    image: Difenda,
+    image: Temp,
     title: "ShopEaze",
     link: "https://github.com/AndreyZIno/ShopEaze",
     langs: ["Java", "Android", "Firebase", "JIRA", "Figma", "Git"],
   },
   {
     id: 2,
-    image: Difenda,
+    image: Temp,
     title: "PriceMe",
     link: "https://github.com/chrisyan04/priceme",
     langs: ["TypeScript", "Next.js", "MongoDB", "Tailwind", "Git", "Vercel"],
   },
   {
     id: 3,
-    image: Difenda,
+    image: Temp,
     title: "UofT Notes",
     link: "https://github.com/chrisyan04/UTSC-Notes",
     langs: ["LaTeX", "Notion", "Pen 🖋️", "Paper 📝", "My ✍🏼", "Love 💚"],
